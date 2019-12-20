7daecb04a43b8bacdf584455f5578bb882e0cb2f
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2019-12-20 11:24:34 +0100

    .suo, storage.ide und 3 weitere dateien aktualisiert...

--- Sharp7/Synchronisiereinrichtung/Display.cs
+++ Sharp7/Synchronisiereinrichtung/Display.cs
@@ -16,7 +16,36 @@ namespace Synchronisiereinrichtung
                 {
                     if (FensterAktiv)
                     {
-                        TxtVentilOeffnung.Text = System.Math.Round(S7Analog.S7_Analog_2_Double(Y, 100),1).ToString() + "%";
+
+                        switch (AuswahlSynchronisierung)
+                        {
+                            case SynchronisierungAuswahl.U_f:
+                                TxtLeistungGenerator.Visibility = System.Windows.Visibility.Hidden;
+                                TxtLeistungNetz.Visibility = System.Windows.Visibility.Hidden;
+                                LblLeistung.Visibility = System.Windows.Visibility.Hidden;
+                                SldNetzLeistung.Visibility = System.Windows.Visibility.Hidden;
+                                Grid.SetRowSpan(RctAuswahl, 13);
+                                break;
+                            case SynchronisierungAuswahl.U_f_Phase:
+                                TxtLeistungGenerator.Visibility = System.Windows.Visibility.Hidden;
+                                TxtLeistungNetz.Visibility = System.Windows.Visibility.Hidden;
+                                LblLeistung.Visibility = System.Windows.Visibility.Hidden;
+                                SldNetzLeistung.Visibility = System.Windows.Visibility.Hidden;
+                                Grid.SetRowSpan(RctAuswahl, 13);
+                                break;
+                            case SynchronisierungAuswahl.U_f_Phase_Leistung:
+                                TxtLeistungGenerator.Visibility = System.Windows.Visibility.Visible;
+                                TxtLeistungNetz.Visibility = System.Windows.Visibility.Visible;
+                                LblLeistung.Visibility = System.Windows.Visibility.Visible;
+                                SldNetzLeistung.Visibility = System.Windows.Visibility.Visible;
+                                Grid.SetRowSpan(RctAuswahl, 15);
+                                break;
+                            default:
+                                break;
+
+                        }
+
+                        TxtVentilOeffnung.Text = System.Math.Round(S7Analog.S7_Analog_2_Double(Y, 100), 1).ToString() + "%";
                         TxtErregerstrom.Text = System.Math.Round(S7Analog.S7_Analog_2_Double(Ie, 10), 1).ToString() + "A";
                         BildEinblenden(Q1, ImgSchalterEin, ImgSchalterAus);
 
@@ -29,6 +58,22 @@ namespace Synchronisiereinrichtung
                         TxtSpannungNetz.Text = "U = " + System.Math.Round(SpannungNetz, 1).ToString() + "V";
                         TxtFrequenzNetz.Text = "f = " + System.Math.Round(FrequenzNetz, 1).ToString() + "Hz";
                         TxtLeistungNetz.Text = "P = " + System.Math.Round(LeistungNetz, 1).ToString() + "W";
+
+                        // Eingaben lesen und übernehmen
+                        UNetz = (short)SldNetzSpannung.Value;
+                        fNetz = (short)SldNetzFrequenz.Value;
+                        PNetz = (short)SldNetzLeistung.Value;
+
+                        SpannungNetz = S7Analog.S7_Analog_2_Double(UNetz, 1000);
+                        FrequenzNetz = S7Analog.S7_Analog_2_Double(fNetz, 100);
+                        LeistungNetz = S7Analog.S7_Analog_2_Double(PNetz, 10000);
+
+                        if (DebugWindowAktiv)
+                        {
+                            Q1 = secondWindow.Q1;
+                            Y = (int)secondWindow.Sld_Y.Value;
+                            Ie = (int)secondWindow.Sld_Ie.Value;
+                        }
                     }
                 });
                 Thread.Sleep(10);
--- Sharp7/Synchronisiereinrichtung/LogikFunktionen.cs
+++ Sharp7/Synchronisiereinrichtung/LogikFunktionen.cs
@@ -14,19 +14,18 @@ namespace Synchronisiereinrichtung
         }
 
 
-        public readonly double Y_n_Faktor = 0.01;
-        public readonly double n_BremsFaktor = 0.92;
+        public readonly double Y_n_Faktor = 0.001;
+        public  double n_BremsFaktor = 0.92;
 
         public SynchronisierungAuswahl AuswahlSynchronisierung;
         public void Logikfunktionen_Task()
         {
             while (FensterAktiv)
             {
-
                 Drehzahl += Y * Y_n_Faktor;
                 Drehzahl *= n_BremsFaktor;
 
-                SpannungGenerator = Drehzahl * S7Analog.S7_Analog_2_Double(Ie, 10) / 10;
+                SpannungGenerator = Drehzahl * S7Analog.S7_Analog_2_Double(Ie, 10) / 20;
                 FrequenzGenerator = Drehzahl / 30;
 
                 n = S7Analog.S7_Analog_2_Short(Drehzahl, 5000);
@@ -35,25 +34,12 @@ namespace Synchronisiereinrichtung
                 UDiff = S7Analog.S7_Analog_2_Short(SpannungDifferenz, 1000);
                 ph = S7Analog.S7_Analog_2_Short(Phasenlage, 1);
 
-                this.Dispatcher.Invoke(() =>
-                               {
-                                   UNetz = (short)SldNetzSpannung.Value;
-                                   fNetz = (short)SldNetzFrequenz.Value;
-                                   PNetz = (short)SldNetzLeistung.Value;
 
-                                   SpannungNetz = S7Analog.S7_Analog_2_Double(UNetz, 1000);
-                                   FrequenzNetz = S7Analog.S7_Analog_2_Double(fNetz, 100);
-                                   LeistungNetz = S7Analog.S7_Analog_2_Double(PNetz, 1000);
 
-                                   if (DebugWindowAktiv)
-                                   {
-                                       Q1 = secondWindow.Q1;
-                                       Y = (int)secondWindow.Sld_Y.Value;
-                                       Ie = (int)secondWindow.Sld_Ie.Value;
-                                   }
-                               });
 
-                Thread.Sleep(100);
+
+
+                Thread.Sleep(10);
             }
         }
 
