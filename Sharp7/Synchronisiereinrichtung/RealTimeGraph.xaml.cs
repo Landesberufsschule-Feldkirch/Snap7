8f7cc6d759c2778fd8c5c59c0941ee628b428e0b
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2020-01-06 18:57:14 +0100

    .suo, storage.ide und 3 weitere dateien aktualisiert...

--- Sharp7/Synchronisiereinrichtung/RealTimeGraph.xaml.cs
+++ Sharp7/Synchronisiereinrichtung/RealTimeGraph.xaml.cs
@@ -11,5 +11,13 @@ namespace Synchronisiereinrichtung
             InitializeComponent();
             viewModel.Start();
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+#pragma warning disable S125 // Sections of code should not be commented out
+            // Application.Current.Shutdown();
+#pragma warning restore S125 // Sections of code should not be commented out
+        }
     }
 }
