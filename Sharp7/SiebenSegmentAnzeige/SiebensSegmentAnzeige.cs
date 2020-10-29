bb26a9107562646200ab57e9bbeaa60ee809ebf3
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2020-10-29 10:27:55 +0100

    7 Segment Anzeige

---
+++ Sharp7/SiebenSegmentAnzeige/SiebensSegmentAnzeige.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SiebenSegmentAnzeige
+{
+
+    public class SiebenSegmentAnzeige : Control
+    {
+        static SiebenSegmentAnzeige()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SiebenSegmentAnzeige), new FrameworkPropertyMetadata(typeof(SiebenSegmentAnzeige)));
+        }
+    }
+}
