1687fbfa24e78aa4c6db92e3782813f23c3fd550
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-03-17 16:12:15 +0100

    LAP 2010 und 2018 auf Testautomat umgestellt

--- Sharp7/LAP_2010_5_Pumpensteuerung/MainWindow.xaml.cs
+++ Sharp7/LAP_2010_5_Pumpensteuerung/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Kommunikation;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
+using BeschriftungPlc;
+using Kommunikation;
 
 namespace LAP_2010_5_Pumpensteuerung
 {
@@ -9,12 +10,13 @@ namespace LAP_2010_5_Pumpensteuerung
         public IPlc Plc { get; set; }
         public string VersionInfoLokal { get; set; }
         public string VersionNummer { get; set; }
-
         public ConfigPlc.Plc ConfigPlc { get; set; }
-        public readonly ViewModel.ViewModel ViewModel;
         public Datenstruktur Datenstruktur { get; set; }
-
+        public TestAutomat.TestAutomat TestAutomat { get; set; }
+        public DisplayPlc.DisplayPlc DisplayPlc { get; set; }
+        public BeschriftungenPlc BeschriftungenPlc { get; set; }
         public DatenRangieren DatenRangieren { get; set; }
+
         private const int AnzByteDigInput = 1;
         private const int AnzByteDigOutput = 1;
         private const int AnzByteAnalogInput = 0;
@@ -28,18 +30,51 @@ namespace LAP_2010_5_Pumpensteuerung
 
             Datenstruktur = new Datenstruktur(AnzByteDigInput, AnzByteDigOutput, AnzByteAnalogInput, AnzByteAnalogOutput);
 
-            ViewModel = new ViewModel.ViewModel(this);
-
-            DatenRangieren = new DatenRangieren(ViewModel);
+            var viewModel = new ViewModel.ViewModel(this);
+            DatenRangieren = new DatenRangieren(viewModel);
 
             InitializeComponent();
-            DataContext = ViewModel;
-
-            Plc = new S71200(Datenstruktur, DatenRangieren.RangierenInput, DatenRangieren.RangierenOutput);
+            DataContext = viewModel;
 
             ConfigPlc = new ConfigPlc.Plc("./ConfigPlc");
+            BeschriftungenPlc = new BeschriftungenPlc();
+            Plc = new S71200(Datenstruktur, DatenRangieren.RangierenInput, DatenRangieren.RangierenOutput);
+            DisplayPlc = new DisplayPlc.DisplayPlc(Datenstruktur, ConfigPlc, BeschriftungenPlc);
 
-            Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.Simulation;
+            TestAutomat = new TestAutomat.TestAutomat(Datenstruktur, DisplayPlc.EventBeschriftungAktualisieren, BeschriftungenPlc);
+            TestAutomat.SetTestConfig("./ConfigTests/");
+            TestAutomat.TabItemFuellen(TabItemAutomatischerSoftwareTest, DisplayPlc);
+
+            Closing += (_, e) =>
+            {
+                e.Cancel = true;
+                Schliessen();
+            };
+        }
+        private void BetriebsartProjektChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is not TabControl tc) return;
+
+            // ReSharper disable once ConvertSwitchStatementToSwitchExpression
+            switch (tc.SelectedIndex)
+            {
+                case 0: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.LaborPlatte; break;
+                case 1: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.Simulation; break;
+                case 2: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.AutomatischerSoftwareTest; break;
+            }
+
+            DisplayPlc.SetBetriebsartProjekt(Datenstruktur);
+        }
+        private void PlcButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (DisplayPlc.FensterAktiv) DisplayPlc.Schliessen();
+            else DisplayPlc.Oeffnen();
+        }
+        private void Schliessen()
+        {
+            DisplayPlc.TaskBeenden();
+            TestAutomat.TaskBeenden();
+            Application.Current.Shutdown();
         }
     }
 }
--- Sharp7/LAP_2018_3_Hydraulikaggregat/MainWindow.xaml.cs
+++ Sharp7/LAP_2018_3_Hydraulikaggregat/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Kommunikation;
-using System.Text;
+﻿using BeschriftungPlc;
+using Kommunikation;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace LAP_2018_3_Hydraulikaggregat
 {
@@ -9,10 +10,13 @@ namespace LAP_2018_3_Hydraulikaggregat
         public IPlc Plc { get; set; }
         public string VersionInfoLokal { get; set; }
         public string VersionNummer { get; set; }
-        public Datenstruktur Datenstruktur { get; set; }
         public ConfigPlc.Plc ConfigPlc { get; set; }
-
+        public Datenstruktur Datenstruktur { get; set; }
+        public TestAutomat.TestAutomat TestAutomat { get; set; }
+        public DisplayPlc.DisplayPlc DisplayPlc { get; set; }
+        public BeschriftungenPlc BeschriftungenPlc { get; set; }
         public DatenRangieren DatenRangieren { get; set; }
+
         private const int AnzByteDigInput = 2;
         private const int AnzByteDigOutput = 2;
         private const int AnzByteAnalogInput = 0;
@@ -32,11 +36,45 @@ namespace LAP_2018_3_Hydraulikaggregat
             InitializeComponent();
             DataContext = viewModel;
 
-            Plc = new S71200(Datenstruktur, DatenRangieren.RangierenInput, DatenRangieren.RangierenOutput);
-
             ConfigPlc = new ConfigPlc.Plc("./ConfigPlc");
+            BeschriftungenPlc = new BeschriftungenPlc();
+            Plc = new S71200(Datenstruktur, DatenRangieren.RangierenInput, DatenRangieren.RangierenOutput);
+            DisplayPlc = new DisplayPlc.DisplayPlc(Datenstruktur, ConfigPlc, BeschriftungenPlc);
 
-            Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.Simulation;
+            TestAutomat = new TestAutomat.TestAutomat(Datenstruktur, DisplayPlc.EventBeschriftungAktualisieren, BeschriftungenPlc);
+            TestAutomat.SetTestConfig("./ConfigTests/");
+            TestAutomat.TabItemFuellen(TabItemAutomatischerSoftwareTest, DisplayPlc);
+
+            Closing += (_, e) =>
+            {
+                e.Cancel = true;
+                Schliessen();
+            };
+        }
+        private void BetriebsartProjektChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is not TabControl tc) return;
+
+            // ReSharper disable once ConvertSwitchStatementToSwitchExpression
+            switch (tc.SelectedIndex)
+            {
+                case 0: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.LaborPlatte; break;
+                case 1: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.Simulation; break;
+                case 2: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.AutomatischerSoftwareTest; break;
+            }
+
+            DisplayPlc.SetBetriebsartProjekt(Datenstruktur);
+        }
+        private void PlcButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (DisplayPlc.FensterAktiv) DisplayPlc.Schliessen();
+            else DisplayPlc.Oeffnen();
+        }
+        private void Schliessen()
+        {
+            DisplayPlc.TaskBeenden();
+            TestAutomat.TaskBeenden();
+            Application.Current.Shutdown();
         }
     }
 }
