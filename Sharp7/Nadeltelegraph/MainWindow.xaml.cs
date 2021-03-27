4d6dd6dc50059e119fd4c7f75c13f403b9c5907f
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-03-27 18:58:57 +0100

    Nadeltelegraph auf TEST umgestellt

--- Sharp7/Nadeltelegraph/MainWindow.xaml.cs
+++ Sharp7/Nadeltelegraph/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Kommunikation;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using BeschriftungPlc;
 
 namespace Nadeltelegraph
 {
@@ -11,7 +13,9 @@ namespace Nadeltelegraph
         public string VersionNummer { get; set; }
         public ConfigPlc.Plc ConfigPlc { get; set; }
         public Datenstruktur Datenstruktur { get; set; }
-
+        public TestAutomat.TestAutomat TestAutomat { get; set; }
+        public DisplayPlc.DisplayPlc DisplayPlc { get; set; }
+        public BeschriftungenPlc BeschriftungenPlc { get; set; }
         public DatenRangieren DatenRangieren { get; set; }
 
         private const int AnzByteDigInput = 1;
@@ -34,11 +38,44 @@ namespace Nadeltelegraph
             InitializeComponent();
             DataContext = viewModel;
 
-            Plc = new S71200(Datenstruktur, DatenRangieren.RangierenInput, DatenRangieren.RangierenOutput);
-
             ConfigPlc = new ConfigPlc.Plc("./ConfigPlc");
+            BeschriftungenPlc = new BeschriftungenPlc();
+            Plc = new S71200(Datenstruktur, DatenRangieren.RangierenInput, DatenRangieren.RangierenOutput);
+            DisplayPlc = new DisplayPlc.DisplayPlc(Datenstruktur, ConfigPlc, BeschriftungenPlc);
 
-            Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.Simulation;
+            TestAutomat = new TestAutomat.TestAutomat(Datenstruktur, DisplayPlc.EventBeschriftungAktualisieren, BeschriftungenPlc);
+            TestAutomat.SetTestConfig("./ConfigTests/");
+            TestAutomat.TabItemFuellen(TabItemAutomatischerSoftwareTest, DisplayPlc);
+
+            Closing += (_, e) =>
+            {
+                e.Cancel = true;
+                Schliessen();
+            };
+        }
+        private void BetriebsartProjektChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is not TabControl tc) return;
+
+            // ReSharper disable once ConvertSwitchStatementToSwitchExpression
+            switch (tc.SelectedIndex)
+            {
+                case 0: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.Simulation; break;
+                case 1: Datenstruktur.BetriebsartProjekt = BetriebsartProjekt.AutomatischerSoftwareTest; break;
+            }
+
+            DisplayPlc.SetBetriebsartProjekt(Datenstruktur);
+        }
+        private void PlcButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (DisplayPlc.FensterAktiv) DisplayPlc.Schliessen();
+            else DisplayPlc.Oeffnen();
+        }
+        private void Schliessen()
+        {
+            DisplayPlc.TaskBeenden();
+            TestAutomat.TaskBeenden();
+            Application.Current.Shutdown();
         }
     }
 }
