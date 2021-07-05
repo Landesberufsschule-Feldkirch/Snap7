9b83a9cf0ac560ad90f9246065236d878a856747
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-07-05 11:13:43 +0200

    Labor Linearachse

--- Sharp7/LaborLinearachse/ViewModel/ViewModel.cs
+++ Sharp7/LaborLinearachse/ViewModel/ViewModel.cs
@@ -14,10 +14,12 @@ namespace LaborLinearachse.ViewModel
             ViAnz = new VisuAnzeigen(mainWindow, Linearachse);
         }
 
-
         private ICommand _btnTaster;
         // ReSharper disable once UnusedMember.Global
         public ICommand BtnTaster => _btnTaster ??= new RelayCommand(ViAnz.Taster);
 
+        private ICommand _btnSchalter;
+        // ReSharper disable once UnusedMember.Global
+        public ICommand BtnSchalter => _btnSchalter ??= new RelayCommand(ViAnz.Schalter);
     }
 }
