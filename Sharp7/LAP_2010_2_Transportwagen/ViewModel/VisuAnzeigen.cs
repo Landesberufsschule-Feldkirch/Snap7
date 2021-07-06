6f83a4fd00a77c69ed39ea223a20fed496c3d323
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-07-06 12:39:10 +0200

    LAP 2010/2 Transportwagen

--- Sharp7/LAP_2010_2_Transportwagen/ViewModel/VisuAnzeigen.cs
+++ Sharp7/LAP_2010_2_Transportwagen/ViewModel/VisuAnzeigen.cs
@@ -27,22 +27,11 @@ namespace LAP_2010_2_Transportwagen.ViewModel
 
             for (var i = 0; i < 100; i++)
             {
-                ClickModeBtn.Add(ClickMode.Press);
+                ClkMode.Add(ClickMode.Press);
+                SichtbarEin.Add(Visibility.Hidden);
+                SichtbarAus.Add(Visibility.Visible);
+                Farbe.Add(Brushes.White);
             }
-
-            ColorF1 = Brushes.LawnGreen;
-            ColorP1 = Brushes.LawnGreen;
-            ColorQ1 = Brushes.LawnGreen;
-            ColorQ2 = Brushes.LawnGreen;
-            ColorS2 = Brushes.LawnGreen;
-
-            VisibilityB1Ein = Visibility.Visible;
-            VisibilityB1Aus = Visibility.Hidden;
-            VisibilityB2Ein = Visibility.Visible;
-            VisibilityB2Aus = Visibility.Hidden;
-
-            VisibilityFuellen = Visibility.Hidden;
-            VisibilityKurzschluss = Visibility.Hidden;
 
             PositionRadLinks = 0;
             PositionRadRechts = 0;
@@ -57,17 +46,16 @@ namespace LAP_2010_2_Transportwagen.ViewModel
 
             while (true)
             {
-                FarbeF1(_transportwagen.F1);
-                FarbeP1(_transportwagen.P1);
-                FarbeQ1(_transportwagen.Q1);
-                FarbeQ2(_transportwagen.Q2);
-                FarbeS2(_transportwagen.S2);
+                FarbeUmschalten(_transportwagen.F1, 3, Brushes.LawnGreen, Brushes.Red);
+                FarbeUmschalten(_transportwagen.P1, 4, Brushes.Red, Brushes.White);
+                FarbeUmschalten(_transportwagen.Q1, 5, Brushes.LawnGreen, Brushes.White);
+                FarbeUmschalten(_transportwagen.Q2, 6, Brushes.LawnGreen, Brushes.White);
+                FarbeUmschalten(_transportwagen.S2, 12, Brushes.LawnGreen, Brushes.Red);
 
-                SichtbarkeitB1(_transportwagen.B1);
-                SichtbarkeitB2(_transportwagen.B2);
-
-                if (_transportwagen.Q1 && _transportwagen.Q2) VisibilityKurzschluss = Visibility.Visible; else VisibilityKurzschluss = Visibility.Hidden;
-                VisibilityFuellen = _transportwagen.Fuellen ? Visibility.Visible : Visibility.Hidden;
+                SichtbarkeitUmschalten(_transportwagen.B1, 1);
+                SichtbarkeitUmschalten(_transportwagen.B2, 2);
+                SichtbarkeitUmschalten(_transportwagen.Fuellen, 20);
+                SichtbarkeitUmschalten(_transportwagen.Q1 && _transportwagen.Q2, 21);
 
                 PositionRadLinks = _transportwagen.Position + abstandRadWagen;
                 PositionRadRechts = _transportwagen.Position + _transportwagen.AbstandRadRechts + abstandRadWagen;
@@ -85,6 +73,41 @@ namespace LAP_2010_2_Transportwagen.ViewModel
                 Thread.Sleep(10);
             }
             // ReSharper disable once FunctionNeverReturns
+        }
+
+
+        internal void FarbeUmschalten(bool val, int i, Brush farbe1, Brush farbe2) => Farbe[i] = val ? farbe1 : farbe2;
+        internal void SichtbarkeitUmschalten(bool val, int i)
+        {
+            SichtbarEin[i] = val ? Visibility.Visible : Visibility.Collapsed;
+            SichtbarAus[i] = val ? Visibility.Collapsed : Visibility.Visible;
+        }
+        internal void Taster(object id)
+        {
+            if (id is not string ascii) return;
+
+            var tasterId = short.Parse(ascii);
+            var gedrueckt = ClickModeButton(tasterId);
+
+            switch (tasterId)
+            {
+                case 11: _transportwagen.S1 = gedrueckt; break;
+                case 13: _transportwagen.S3 = gedrueckt; break;
+                default: throw new ArgumentOutOfRangeException(nameof(id));
+            }
+        }
+        internal void Schalter(object id)
+        {
+            if (id is not string ascii) return;
+
+            var schalterId = short.Parse(ascii);
+
+            switch (schalterId)
+            {
+                case 12: _transportwagen.S2 = !_transportwagen.S2; break;
+                case 3: _transportwagen.F1 = !_transportwagen.F1; break;
+                default: throw new ArgumentOutOfRangeException(nameof(id));
+            }
         }
 
         #region SPS Version, Status und Farbe
@@ -145,161 +168,6 @@ namespace LAP_2010_2_Transportwagen.ViewModel
         }
         #endregion SPS Versionsinfo, Status und Farbe
 
-        #region Farben
-        public void FarbeF1(bool val) => ColorF1 = val ? Brushes.LawnGreen : Brushes.Red;
-        private Brush _colorF1;
-        public Brush ColorF1
-        {
-            get => _colorF1;
-            set
-            {
-                _colorF1 = value;
-                OnPropertyChanged(nameof(ColorF1));
-            }
-        }
-
-        public void FarbeP1(bool val) => ColorP1 = val ? Brushes.Red : Brushes.White;
-        private Brush _colorP1;
-        public Brush ColorP1
-        {
-            get => _colorP1;
-            set
-            {
-                _colorP1 = value;
-                OnPropertyChanged(nameof(ColorP1));
-            }
-        }
-
-        public void FarbeQ1(bool val) => ColorQ1 = val ? Brushes.LawnGreen : Brushes.White;
-        private Brush _colorQ1;
-        public Brush ColorQ1
-        {
-            get => _colorQ1;
-            set
-            {
-                _colorQ1 = value;
-                OnPropertyChanged(nameof(ColorQ1));
-            }
-        }
-
-        public void FarbeQ2(bool val) => ColorQ2 = val ? Brushes.LawnGreen : Brushes.White;
-        private Brush _colorQ2;
-        public Brush ColorQ2
-        {
-            get => _colorQ2;
-            set
-            {
-                _colorQ2 = value;
-                OnPropertyChanged(nameof(ColorQ2));
-            }
-        }
-
-        public void FarbeS2(bool val) => ColorS2 = val ? Brushes.LawnGreen : Brushes.Red;
-        private Brush _colorS2;
-        public Brush ColorS2
-        {
-            get => _colorS2;
-            set
-            {
-                _colorS2 = value;
-                OnPropertyChanged(nameof(ColorS2));
-            }
-        }
-        #endregion Farben
-
-        #region Sichtbarkeit 
-        public void SichtbarkeitB1(bool val)
-        {
-            if (val)
-            {
-                VisibilityB1Ein = Visibility.Visible;
-                VisibilityB1Aus = Visibility.Hidden;
-            }
-            else
-            {
-                VisibilityB1Ein = Visibility.Hidden;
-                VisibilityB1Aus = Visibility.Visible;
-            }
-        }
-        private Visibility _visibilityB1Ein;
-        public Visibility VisibilityB1Ein
-        {
-            get => _visibilityB1Ein;
-            set
-            {
-                _visibilityB1Ein = value;
-                OnPropertyChanged(nameof(VisibilityB1Ein));
-            }
-        }
-
-        private Visibility _visibilityB1Aus;
-        public Visibility VisibilityB1Aus
-        {
-            get => _visibilityB1Aus;
-            set
-            {
-                _visibilityB1Aus = value;
-                OnPropertyChanged(nameof(VisibilityB1Aus));
-            }
-        }
-
-        public void SichtbarkeitB2(bool val)
-        {
-            if (val)
-            {
-                VisibilityB2Ein = Visibility.Visible;
-                VisibilityB2Aus = Visibility.Hidden;
-            }
-            else
-            {
-                VisibilityB2Ein = Visibility.Hidden;
-                VisibilityB2Aus = Visibility.Visible;
-            }
-        }
-        private Visibility _visibilityB2Ein;
-        public Visibility VisibilityB2Ein
-        {
-            get => _visibilityB2Ein;
-            set
-            {
-                _visibilityB2Ein = value;
-                OnPropertyChanged(nameof(VisibilityB2Ein));
-            }
-        }
-        private Visibility _visibilityB2Aus;
-        public Visibility VisibilityB2Aus
-        {
-            get => _visibilityB2Aus;
-            set
-            {
-                _visibilityB2Aus = value;
-                OnPropertyChanged(nameof(VisibilityB2Aus));
-            }
-        }
-
-        private Visibility _visibilityKurzschluss;
-        public Visibility VisibilityKurzschluss
-        {
-            get => _visibilityKurzschluss;
-            set
-            {
-                _visibilityKurzschluss = value;
-                OnPropertyChanged(nameof(VisibilityKurzschluss));
-            }
-        }
-
-        private Visibility _visibilityFuellen;
-        public Visibility VisibilityFuellen
-        {
-            get => _visibilityFuellen;
-            set
-            {
-                _visibilityFuellen = value;
-                OnPropertyChanged(nameof(VisibilityFuellen));
-            }
-        }
-        #endregion
-
         #region Position
         private double _positionRadLinks;
         public double PositionRadLinks
@@ -335,34 +203,44 @@ namespace LAP_2010_2_Transportwagen.ViewModel
         }
         #endregion Position
 
+        #region Sichtbarkeit
+        private ObservableCollection<Visibility> _sichtbarEin = new();
+        public ObservableCollection<Visibility> SichtbarEin
+        {
+            get => _sichtbarEin;
+            set
+            {
+                _sichtbarEin = value;
+                OnPropertyChanged(nameof(SichtbarEin));
+            }
+        }
+
+        private ObservableCollection<Visibility> _sichtbarAus = new();
+        public ObservableCollection<Visibility> SichtbarAus
+        {
+            get => _sichtbarAus;
+            set
+            {
+                _sichtbarAus = value;
+                OnPropertyChanged(nameof(SichtbarAus));
+            }
+        }
+        #endregion
+
+        #region Farbe
+        private ObservableCollection<Brush> _farbe = new();
+        public ObservableCollection<Brush> Farbe
+        {
+            get => _farbe;
+            set
+            {
+                _farbe = value;
+                OnPropertyChanged(nameof(Farbe));
+            }
+        }
+        #endregion
+
         #region Taster/Schalter
-        internal void Taster(object id)
-        {
-            if (id is not string ascii) return;
-
-            var tasterId = short.Parse(ascii);
-            var gedrueckt = ClickModeButton(tasterId);
-
-            switch (tasterId)
-            {
-                case 1: _transportwagen.S1 = gedrueckt; break;
-                case 3: _transportwagen.S3 = gedrueckt; break;
-                default: throw new ArgumentOutOfRangeException(nameof(id));
-            }
-        }
-        internal void Schalter(object id)
-        {
-            if (id is not string ascii) return;
-
-            var schalterId = short.Parse(ascii);
-
-            switch (schalterId)
-            {
-                case 2: _transportwagen.S2 = !_transportwagen.S2; break;
-                case 5: _transportwagen.F1 = !_transportwagen.F1; break;
-                default: throw new ArgumentOutOfRangeException(nameof(id));
-            }
-        }
         public bool ClickModeButton(int tasterId)
         {
             if (ClkMode[tasterId] == ClickMode.Press)
@@ -375,24 +253,21 @@ namespace LAP_2010_2_Transportwagen.ViewModel
             return false;
         }
 
-        private ObservableCollection<ClickMode> _clickModeBtn = new();
-        public ObservableCollection<ClickMode> ClickModeBtn
+        private ObservableCollection<ClickMode> _clkMode = new();
+        public ObservableCollection<ClickMode> ClkMode
         {
-            get => _clickModeBtn;
+            get => _clkMode;
             set
             {
-                _clickModeBtn = value;
-                OnPropertyChanged(nameof(ClickModeBtn));
+                _clkMode = value;
+                OnPropertyChanged(nameof(ClkMode));
             }
         }
         #endregion Taster/Schalter
 
         #region iNotifyPeropertyChanged Members
-
         public event PropertyChangedEventHandler PropertyChanged;
-
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
         #endregion iNotifyPeropertyChanged Members
     }
 }
